3bce3e0c64929bcda16d6ca3534cadfe93b9ac33
hgaafar <hgaafar@banquemisr.com>
2022-06-25 19:34:30 +0200

    add unit test

--- src/test/java/com/jenikens/demo/DemoApplicationTests.java
+++ src/test/java/com/jenikens/demo/DemoApplicationTests.java
@@ -1,13 +1,40 @@
 package com.jenikens.demo;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void getName() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/getName")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andReturn();
+	}
+
+	@Test
+	public void returnsNotFoundForInvalidSingleRide() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/getName/3")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andReturn();
 	}
 
 }
